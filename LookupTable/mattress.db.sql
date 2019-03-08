5b406b8beb2624a5538ea57b4d3a804a127ec174
JMaldonado91 <39070263+JMaldonado91@users.noreply.github.com>
2019-03-08 10:55:35 -0500

    Update mattress.db.sql

--- LookupTable/mattress.db.sql
+++ LookupTable/mattress.db.sql
@@ -1,15 +1,28 @@
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",1992
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",1993
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",2001
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",2002
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",2003
-Deluxe,10,firm,yes,Twin-Cal King,81-160,"1,434-2,399",2003
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",1994
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",1995
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",1996
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",1997
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",1998
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",1999
-Classic,8,firm,yes,Twin-Cal King,62-80,"1,184-1,999",2000
-Celebrity/Supreme PillowTop,13,medium soft,yes cashmere cover,"Twin Long, Queen, King, Cal King",,"2,184-3,299",2003
-Deluxe,10,firm,yes,Twin-Cal King,81-160,"1,434-2,399",2002
+CREATE DATABASE mattress
+
+USE mattress
+
+CREATE TABLE mattress_lookup 
+(mattress VARCHAR NOT NULL PRIMARY KEY,
+feel VARCHAR NULL,
+year INTEGER NULL,
+profile INTEGER NULL)
+
+INSERT mattress_lookup
+(mattress, feel, year, profile)
+VALUES
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,1992),
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,1993),
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,2001),
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,2002),
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,2003),
+(Deluxe,10,firm,yes,Twin-Cal King,81-160,1,434-2,399,2003),
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,1994),
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,1995),
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,1996),
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,1997),
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,1998),
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,1999),
+(Classic,8,firm,yes,Twin-Cal King,62-80,1,184-1,999,2000),
+(Celebrity/Supreme PillowTop,13,medium soft,yes cashmere cover,"Twin Long, Queen, King, Cal King",,2,184-3,299,2003),
+(Deluxe,10,firm,yes,Twin-Cal King,81-160,1,434-2,399,2002);
